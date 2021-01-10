7a431c05d49d2c582b6c340edb29aa0b290b5ffb
SteveYoungYc <46893507+SteveYoungYc@users.noreply.github.com>
2021-01-10 22:27:29 +0800

    炮塔发射激光

--- Assets/Scripts/TurretShoot.cs
+++ Assets/Scripts/TurretShoot.cs
@@ -7,10 +7,20 @@ public class TurretShoot : MonoBehaviour {
     private GameObject gunBarrel;
     private Vector3 toward;
     private Ray ray;
+    
+    private LineRenderer laser;
     // Start is called before the first frame update
     void Start() {
         barrel = GameObject.FindGameObjectWithTag("TurretBarrel");
         gunBarrel = GameObject.Find("Cylinder");
+        
+        laser = GetComponent<LineRenderer>();
+        laser.positionCount = 2;
+        laser.startWidth = 0.08f;
+        laser.endWidth = 0.08f;
+        laser.startColor = Color.magenta;
+        laser.endColor = Color.cyan;
+        laser.SetPosition(0, barrel.transform.position);
     }
 
     // Update is called once per frame
@@ -21,8 +31,9 @@ public class TurretShoot : MonoBehaviour {
         ray = new Ray(gunBarrel.transform.position, Vector3.Normalize(toward));
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit, Mathf.Infinity)) {
-            Debug.Log("碰撞对象: " + hit.collider.name);
-            Debug.DrawLine(ray.origin, hit.point, Color.red);
+            //Debug.Log("碰撞对象: " + hit.collider.name);
+            //Debug.DrawLine(ray.origin, hit.point, Color.red);
+            laser.SetPosition(1, hit.point);
         }
     }
 }
