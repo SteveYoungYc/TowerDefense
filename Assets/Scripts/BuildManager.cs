2dae7c7ae560b6701410ca3e447d2a436ec39746
SteveYoungYc <46893507+SteveYoungYc@users.noreply.github.com>
2021-01-12 18:15:17 +0800

    用鼠标点击地图可以生成炮台

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BuildManager : MonoBehaviour {
+    public GameObject turretPrefab;
+    // Start is called before the first frame update
+    void Start() {
+        //turretPrefab = Resources.Load<GameObject>("Prefabs/Turret");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            if (EventSystem.current.IsPointerOverGameObject() == false) {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                bool isCollider = Physics.Raycast(ray, out hit, 1000, LayerMask.GetMask("Map"));
+                if (isCollider) {
+                    Instantiate(turretPrefab, hit.point, Quaternion.identity);
+                }
+            }
+        }
+    }
+}
