0d2cbddec1a3b140e08b2d16385bf2f9681f8c0a
SteveYoungYc <46893507+SteveYoungYc@users.noreply.github.com>
2021-01-13 22:24:53 +0800

    导入资源前备份

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -5,16 +5,19 @@ using UnityEngine.EventSystems;
 
 public class BuildManager : MonoBehaviour {
     public GameObject turretPrefab;
+
+    private Camera mainCamera;
     // Start is called before the first frame update
     void Start() {
         //turretPrefab = Resources.Load<GameObject>("Prefabs/Turret");
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
             if (EventSystem.current.IsPointerOverGameObject() == false) {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hit;
                 bool isCollider = Physics.Raycast(ray, out hit, 1000, LayerMask.GetMask("Map"));
                 if (isCollider) {
