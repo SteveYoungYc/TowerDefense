73e2f936062819ad6ceb81c5267cc21bb4a53212
Yc <STM32_Yc@sjtu.edu.cn>
2021-01-25 22:54:05 +0800

    从三个出生点分别生成僵尸

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -8,11 +8,11 @@ public class EnemyGenerator : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public AnimatorController animatorController;
-    private GameObject[] enemyClone;
-    private int enemyNum = 100;
-    private Animator[] enemyAnimator;
+    private GameObject[,] enemyClone;
+    private int enemyNum = 10;
+    private Animator[,] enemyAnimator;
 
-    private NavMeshAgent[] navAgents;
+    private NavMeshAgent[,] navAgents;
     private Camera mainCamera;
 
     private int bornPointsNum = 3;
@@ -23,9 +23,9 @@ public class EnemyGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        enemyClone = new GameObject[enemyNum];
-        enemyAnimator = new Animator[enemyNum];
-        navAgents = new NavMeshAgent[enemyNum];
+        enemyClone = new GameObject[bornPointsNum, enemyNum];
+        enemyAnimator = new Animator[bornPointsNum, enemyNum];
+        navAgents = new NavMeshAgent[bornPointsNum, enemyNum];
         bornPoints = new Vector3[bornPointsNum];
 
         destination = new Vector3(-24.83f, 0.99f, -58.83f);
@@ -33,17 +33,22 @@ public class EnemyGenerator : MonoBehaviour
         bornPoints[1] = new Vector3(44.11f, 3.17f, 23.33f);
         bornPoints[2] = new Vector3(92.40f, 3.17f, -20.53f);
 
-        for (int i = 0; i < enemyNum; i++)
+        for (int i = 0; i < bornPointsNum; i++)
         {
-            enemyClone[i] = Instantiate(enemyPrefab, bornPoints[0], Quaternion.identity);
-            enemyClone[i].tag = "Enemy";
-            enemyAnimator[i] = enemyClone[i].GetComponent<Animator>();
-            enemyAnimator[i].runtimeAnimatorController = animatorController;
-            enemyClone[i].AddComponent<NavMeshAgent>();
-            navAgents[i] = enemyClone[i].GetComponent<NavMeshAgent>();
+            for (int j = 0; j < enemyNum; j++)
+            {
+                enemyClone[i, j] = Instantiate(enemyPrefab, bornPoints[i], Quaternion.identity);
+                enemyClone[i, j].tag = "Enemy";
+                enemyAnimator[i, j] = enemyClone[i, j].GetComponent<Animator>();
+                enemyAnimator[i, j].runtimeAnimatorController = animatorController;
+                enemyClone[i, j].AddComponent<NavMeshAgent>();
+                navAgents[i, j] = enemyClone[i, j].GetComponent<NavMeshAgent>();
+                navAgents[i, j].destination = destination;
+            }
         }
 
         mainCamera = Camera.main;
+        print("Start finished");
     }
 
     // Update is called once per frame
@@ -61,18 +66,20 @@ public class EnemyGenerator : MonoBehaviour
             bool isCollider = Physics.Raycast(ray, out hit, 1000, LayerMask.GetMask("Map"));
             if (isCollider)
             {
+                foreach (var nav in navAgents)
+                {
+                    nav.SetDestination(hit.point);
+                }
             }
         }
 
-        foreach (var nav in navAgents)
+        for (int i = 0; i < bornPointsNum; i++)
         {
-            nav.SetDestination(destination);
-        }
-
-        for (int i = 0; i < enemyNum; i++)
-        {
-            enemyAnimator[i].SetBool("startRun",
-                !(Vector3.Distance(enemyClone[i].transform.position, navAgents[i].destination) < 0.3 * enemyNum));
+            for (int j = 0; j < enemyNum; j++)
+            {
+                enemyAnimator[i, j].SetBool("startRun",
+                    !(Vector3.Distance(enemyClone[i, j].transform.position, navAgents[i, j].destination) < 0.3 * enemyNum));
+            }
         }
     }
 }
