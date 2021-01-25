bd93c7a0cc0baf76f6767012bc2944c73dfabbfa
Yc <STM32_Yc@sjtu.edu.cn>
2021-01-25 22:25:45 +0800

    从出生点生成100个僵尸

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -8,16 +8,17 @@ public class EnemyGenerator : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public AnimatorController animatorController;
-
     private GameObject[] enemyClone;
-
-    private int enemyNum = 3;
-
+    private int enemyNum = 100;
     private Animator[] enemyAnimator;
 
     private NavMeshAgent[] navAgents;
-
     private Camera mainCamera;
+
+    private int bornPointsNum = 3;
+    private Vector3 destination;
+
+    private Vector3[] bornPoints;
 
     // Start is called before the first frame update
     void Start()
@@ -25,11 +26,17 @@ public class EnemyGenerator : MonoBehaviour
         enemyClone = new GameObject[enemyNum];
         enemyAnimator = new Animator[enemyNum];
         navAgents = new NavMeshAgent[enemyNum];
-        enemyClone[0] = Instantiate(enemyPrefab, new Vector3(0, -0.1f, 0), Quaternion.identity);
-        enemyClone[1] = Instantiate(enemyPrefab, new Vector3(2, -0.1f, 0), Quaternion.identity);
-        enemyClone[2] = Instantiate(enemyPrefab, new Vector3(4, -0.1f, 0), Quaternion.identity);
+        bornPoints = new Vector3[bornPointsNum];
+
+        destination = new Vector3(-24.83f, 0.99f, -58.83f);
+        bornPoints[0] = new Vector3(86.16f, 2.99f, 22.39f);
+        bornPoints[1] = new Vector3(44.11f, 3.17f, 23.33f);
+        bornPoints[2] = new Vector3(92.40f, 3.17f, -20.53f);
+
         for (int i = 0; i < enemyNum; i++)
         {
+            enemyClone[i] = Instantiate(enemyPrefab, bornPoints[0], Quaternion.identity);
+            enemyClone[i].tag = "Enemy";
             enemyAnimator[i] = enemyClone[i].GetComponent<Animator>();
             enemyAnimator[i].runtimeAnimatorController = animatorController;
             enemyClone[i].AddComponent<NavMeshAgent>();
@@ -54,11 +61,12 @@ public class EnemyGenerator : MonoBehaviour
             bool isCollider = Physics.Raycast(ray, out hit, 1000, LayerMask.GetMask("Map"));
             if (isCollider)
             {
-                foreach (var nav in navAgents)
-                {
-                    nav.SetDestination(hit.point);
-                }
             }
+        }
+
+        foreach (var nav in navAgents)
+        {
+            nav.SetDestination(destination);
         }
 
         for (int i = 0; i < enemyNum; i++)
